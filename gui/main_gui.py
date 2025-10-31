0d8af5b138e0a0ef30c86355380f694ae60ff01f
DcF0rd <zachary.ford8@gmail.com>
2025-10-31 20:29:44 +1000

    Initial GUI skeleton

---
+++ gui/main_gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("NEXUM-CHECKPOINT")
+root.geometry("600x400")
+
+label = tk.Label(root, text="Welcome to NEXUM-CHECKPOINT")
+label.pack(pady=20)
+
+root.mainloop()
